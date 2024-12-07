2a9b6eb61a3460481f63a66ffc4d2d2aa86af4fb
nekopiiie <kayrise@bk.ru>
2024-12-08 01:05:59 +0300

    11_1

---
+++ all_homework/task_11_1/task_11_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "f.txt";
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            writer.WriteLine("x\tsin(x)");
+
+            for (double x = 0; x <= 1; x += 0.1)
+            {
+                double sinValue = Math.Sin(x);
+                writer.WriteLine($"{x:F1}\t{sinValue:F4}"); 
+            }
+        }
+        Console.WriteLine($"Таблица значений sin(x) записана в файл {filePath}");
+    }
+}
