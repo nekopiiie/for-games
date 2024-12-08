a96838726a266f18eec5842c9711ecc4b27054d8
nekopiiie <kayrise@bk.ru>
2024-12-08 15:25:26 +0300

    доп задание 7 лабиринт

---
+++ all_homework/dop_task_7_labyrinth/Labyrinth/Player.cs
@@ -0,0 +1,26 @@
+﻿public class Player
+{
+    public int X { get; private set; }
+    public int Y { get; private set; }
+
+    public Player(int startX, int startY)
+    {
+        X = startX;
+        Y = startY;
+    }
+
+    public void Move(int deltaX, int deltaY, char[,] labyrinth)
+    {
+        int newX = X + deltaX;
+        int newY = Y + deltaY;
+
+        // Проверка на границы и стены лабиринта
+        if (newX >= 0 && newX < labyrinth.GetLength(0) &&
+            newY >= 0 && newY < labyrinth.GetLength(1) &&
+            labyrinth[newX, newY] == ' ')
+        {
+            X = newX;
+            Y = newY;
+        }
+    }
+}
